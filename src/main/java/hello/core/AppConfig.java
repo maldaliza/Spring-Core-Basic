62a16e079874226c875ff3125ce17b961978c47d
jaekook <jaekook4849@gmail.com>
2021-02-02 10:28:29 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,25 +1,33 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+/*
+    생성자 주입 -> "관심사 분리"(클래스간 결합도를 낮춘다)
+*/
 public class AppConfig {
-    /*
-        생성자 주입 -> "관심사 분리"(클래스간 결합도를 낮춘다)
-    */
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
-        );
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
