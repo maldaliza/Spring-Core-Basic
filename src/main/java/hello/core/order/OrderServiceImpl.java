9862e34d7ef8c734918741fbe367581f85a2a6dc
jaekook <jaekook4849@gmail.com>
2021-02-06 04:39:07 +0900

    When type matching with Autowired, if the result is more than 2, using @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,15 +5,22 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // 생성자 주입을 하면 final 키워드 사용가능! (생성할 때 정해지면 바뀌지 않는다.)
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired      // 생성자가 딱 1개이므로 생략 가능! (생성자 주입)
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     /*
         ### 롬복 라이브러리로 final 키워드(필수값)가 붙은 값을 파라미터로 받는 생성자를 자동으로 생성! ###
